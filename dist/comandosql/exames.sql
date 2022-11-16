5ef577fc5095c41c8fa372f244afdd87fac0cfb9
ksan222 <ramoscaio69@gmail.com>
2022-11-16 10:29:43 -0300

    Update exames.sql

--- dist/comandosql/exames.sql
+++ dist/comandosql/exames.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Nov-2022 às 04:30
--- Versão do servidor: 10.4.25-MariaDB
--- versão do PHP: 7.4.30
+-- Tempo de geração: 16-Nov-2022 às 14:27
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cadastro_paciente` (
-  `id` int(255) NOT NULL,
+  `id` int(50) NOT NULL,
   `paciente` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nacionalidade` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `idade` int(3) NOT NULL,
-  `data` date NOT NULL DEFAULT current_timestamp(),
+  `nacionalidade` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idade` int(5) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
   `peso` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -41,13 +41,8 @@ CREATE TABLE `cadastro_paciente` (
 --
 
 INSERT INTO `cadastro_paciente` (`id`, `paciente`, `nacionalidade`, `idade`, `data`, `peso`) VALUES
-(10, '[paciente]', '[nacionalidade]', 0, '0000-00-00', '[peso]'),
-(11, '[paciente]', '[nacionalidade]', 0, '0000-00-00', '[peso]'),
-(12, 'Bianca', 'Ramos', 23, '2022-11-15', '68,600'),
-(13, 'Caio Ramos de Lima', 'Brasil', 22, '9910-08-07', ''),
-(14, 'Biaca de Matos Bueno', 'Brasil', 23, '1998-08-28', '68'),
-(15, 'Caio Ramos de Lima', 'Brasil', 22, '2000-08-07', '69,800'),
-(16, 'Caio Ramos de Lima', 'Brasil', 22, '2000-08-07', '69,800');
+(4, 'Gustavo ', 'Bolivia', 20, '2022-11-21 00:00:00', '60'),
+(5, 'Luis Carlos', 'Bolivia', 32, '2022-11-07 00:00:00', '78');
 
 -- --------------------------------------------------------
 
@@ -57,19 +52,19 @@ INSERT INTO `cadastro_paciente` (`id`, `paciente`, `nacionalidade`, `idade`, `da
 
 CREATE TABLE `capa` (
   `id_capa` int(50) NOT NULL,
-  `path` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `data_envio` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `nome` int(100) NOT NULL,
+  `path` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data_envio` int(50) NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `capa`
 --
 
-INSERT INTO `capa` (`id_capa`, `path`, `data_envio`, `nome`) VALUES
-(1, 'arquivo/636b13504a66d.png', '2022-11-08 23:41:20.307017', 'MATRIX.png'),
-(2, 'arquivo/636b14227101e.pdf', '2022-11-08 23:44:50.463670', 'Curriculo Caio.pdf'),
-(3, 'arquivo/636b1532c5e4d.pdf', '2022-11-08 23:49:22.812654', 'Curriculo Caio.pdf');
+INSERT INTO `capa` (`id_capa`, `nome`, `path`, `data_envio`) VALUES
+(1, 0, '', 2147483647),
+(2, 0, '', 2147483647),
+(3, 0, '', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -78,14 +73,22 @@ INSERT INTO `capa` (`id_capa`, `path`, `data_envio`, `nome`) VALUES
 --
 
 CREATE TABLE `editar_paciente` (
-  `cpf_paciente` int(10) NOT NULL,
-  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `idade` int(5) NOT NULL,
-  `nacionalidade` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cpf_paciente` int(15) NOT NULL,
+  `nome` int(5) NOT NULL,
+  `idade` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nacionalidade` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `bairro` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `cidade` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `endereco` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `endereco` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `editar_paciente`
+--
+
+INSERT INTO `editar_paciente` (`cpf_paciente`, `nome`, `idade`, `nacionalidade`, `bairro`, `cidade`, `endereco`) VALUES
+(0, 25, 'Br', 'VG', 'Pinhais', 'Rua', 0),
+(2147483647, 0, '22', 'BR', 'Pinhais', 'Vargem grande', 0);
 
 -- --------------------------------------------------------
 
@@ -103,7 +106,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`email`, `senha`) VALUES
-('ramoscaio69@gmail.com', '$2y$10$Z97t.i4V5erdMK9Ho0koH.sxWkGHjPRvH.yWtMW8Y1Dt2FW24Xqdu');
+('ramoscaio69@gmail.com', '$2y$10$9xe8.VEPkFn6X2wQ9hAdz.FyQHI33bMief4AIj2HK87VFiRXtluTC'),
+('alvarogustavo@gmail.com', '$2y$10$z/efKFc7HWbgcV4YyQHzmeWDaCi5QdgguxbO..cOMLmyC1DAzbIVu'),
+('alvarogustavo@gmail.com', '$2y$10$SHy6LEucD.tyoXh9VC2OKeTwHf5eTSYjhA8cf5vb9RIG.644CP63a');
 
 --
 -- Índices para tabelas despejadas
@@ -135,19 +140,13 @@ ALTER TABLE `editar_paciente`
 -- AUTO_INCREMENT de tabela `cadastro_paciente`
 --
 ALTER TABLE `cadastro_paciente`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `capa`
 --
 ALTER TABLE `capa`
   MODIFY `id_capa` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `editar_paciente`
---
-ALTER TABLE `editar_paciente`
-  MODIFY `cpf_paciente` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
