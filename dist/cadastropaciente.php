5484fc69a9fc75caa27a88fcd5f06ff15974336e
ksan222 <ramoscaio69@gmail.com>
2022-11-23 09:38:32 -0300

    Update cadastropaciente.php

--- dist/cadastropaciente.php
+++ dist/cadastropaciente.php
@@ -46,7 +46,7 @@
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="index.php">
+                        <a class="nav-link" href="inicio.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Inicio
                         </a>
